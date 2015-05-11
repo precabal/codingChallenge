44368b3516b6de677dc72172fa8b46a65dc0576b
precabal <pablorecabal@gmail.com>
2015-05-11 19:27:48 -0400

    modified script to compile and run solution

--- run.sh
+++ run.sh
@@ -3,17 +3,17 @@
 # run script for running the word count
 
 # first I'll make sure that all my programs have the proper permissions
-chmod a+x src/WordCount.java
-chmod a+x .
+chmod a+x src/codingChallenge/*.java
+chmod a+x src/codingChallenge/
 
 # then, I'll create the output directory if it hasn't been created
-#mkdir ./wc_output/
+# mkdir ./wc_output/
 
 # then I'll compile the program
-javac ./src/WordCount.java -d .
+javac ./src/codingChallenge/*.java
 
 # finally, I execute my program
-java WordCount ./wc_input/ ./wc_output/
+java -cp ./src/ codingChallenge.WordCountStats ./wc_input/ ./wc_output/
 
 
 
